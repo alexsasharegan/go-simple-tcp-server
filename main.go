3b0984e1b6992c0368a775478f145f222f776809
Alex Regan <alex.joseph.regan@gmail.com>
2017-11-03 08:06:08 -0700

    feat: add sized wait group limiting and shutdown logic

--- main.go
+++ main.go
@@ -2,49 +2,91 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/alexsasharegan/go-simple-tcp-server/sync"
 )
 
 const (
-	CONN_HOST = "localhost"
-	CONN_PORT = "3280"
-	CONN_TYPE = "tcp"
+	host      = "localhost"
+	port      = "3280"
+	connLimit = 6
 )
 
 func main() {
-	// Listen for incoming connections.
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	handleSignalClose()
+	l, err := net.Listen("tcp", host+":"+port)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
+		log.Fatalf("Error listening: %v", err)
 	}
-	// Close the listener when the application closes.
+
+	fmt.Println("Listening on " + host + ":" + port)
 	defer l.Close()
-	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+
+	swg := sync.NewSizedWaitGroup(connLimit)
+
 	for {
-		// Listen for an incoming connection.
+		swg.Add(1)
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			os.Exit(1)
+			log.Fatalf("Error accepting connection: %v", err)
 		}
-		// Handle connections in a new goroutine.
-		go handleRequest(conn)
+
+		go func() {
+			defer swg.Done()
+			handleRequest(conn)
+		}()
 	}
 }
 
 // Handles incoming requests.
 func handleRequest(conn net.Conn) {
+	defer conn.Close()
 	// Make a buffer to hold incoming data.
 	buf := make([]byte, 1024)
 	// Read the incoming connection into the buffer.
-	reqLen, err := conn.Read(buf)
+	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		log.Fatalf("Error reading: %v", err)
 	}
-	// Send a response back to person contacting us.
-	conn.Write([]byte("Message received."))
-	// Close the connection when you're done with it.
-	conn.Close()
+	if err != nil {
+		log.Fatalf("could not parse request: %v", err)
+	}
+	conn.Write([]byte(fmt.Sprintf("Message received: %s", string(buf))))
+	err = logToFile(buf)
+	if err != nil {
+		log.Fatalf("could not log request: %v", err)
+	}
+}
+
+func logToFile(b []byte) error {
+	f, err := os.OpenFile(fmt.Sprintf("data.%d.log", 0), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("could not open log file: %v", err)
+	}
+	defer f.Close()
+
+	f.Write(b)
+	return nil
+}
+
+// handleSignalClose sets up a signal listener for shutdown signals.
+// It returns a bool channel to communicate shutdown and allow the caller to perform tear down.
+func handleSignalClose() <-chan bool {
+	sigc := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigc, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		<-sigc
+		fmt.Println("graceful shutdown")
+		done <- true
+		os.Exit(0)
+	}()
+
+	return done
 }
